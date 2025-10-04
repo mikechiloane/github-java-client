a195513d7f8f2003e206879ab222330ab2f8d364
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-04 23:56:40 +0200

    Introduce commit Action

---
+++ src/main/java/com/recceda/action/CommitAction.java
@@ -0,0 +1,11 @@
+package com.recceda.action;
+
+import com.recceda.http.Client;
+
+public class CommitAction {
+    private final Client client;
+
+    public CommitAction(Client client){
+        this.client = client;
+    }
+}
