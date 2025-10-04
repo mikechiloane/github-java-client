b6fcdee7decffa82b8774ba9c72662713916f1ea
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-05 01:57:48 +0200

    Add create repository request

---
+++ src/main/java/com/recceda/requests/repository/CreateRepositoryRequest.java
@@ -0,0 +1,11 @@
+package com.recceda.requests.repository;
+
+
+public class CreateRepositoryRequest {
+    private String name;
+    private String description;
+    private String homepage;
+    private boolean isPrivate;
+    private String isTemplate;
+
+}
