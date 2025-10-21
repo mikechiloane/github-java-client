a72dc6fa2ccf4f6f03f202c256a489cd0079255a
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-21 23:54:56 +0200

    Refactor

--- src/main/java/com/recceda/action/CommitAction.java
+++ src/main/java/com/recceda/action/CommitAction.java
@@ -1,11 +1,29 @@
 package com.recceda.action;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.recceda.elements.Commit;
+import com.recceda.http.ApiPaths;
 import com.recceda.http.Client;
+import com.recceda.mapper.ResponseMapper;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
 
 public class CommitAction {
     private final Client client;
 
     public CommitAction(Client client){
         this.client = client;
+    }
+
+    public Commit getCommit(String owner, String repo, String sha) throws ExecutionException, InterruptedException, JsonProcessingException {
+        String path = ApiPaths.SLASH + ApiPaths.REPOS + ApiPaths.SLASH + owner + ApiPaths.SLASH + repo + ApiPaths.SLASH + ApiPaths.COMMITS + ApiPaths.SLASH + sha;
+        HttpRequest request = client.requestBuilder(path).build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
+        if (response.statusCode() != 200) {
+            throw new RuntimeException("Failed to get commit");
+        }
+        return ResponseMapper.fromResponse(response, Commit.class);
     }
 }
