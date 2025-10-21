6dc7ab3c576d253148d8bd916a82ec9692df48da
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-21 23:48:07 +0200

    Refactor

---
+++ src/main/java/com/recceda/action/FileAction.java
@@ -0,0 +1,68 @@
+package com.recceda.action;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.recceda.elements.Repository;
+import com.recceda.http.ApiPaths;
+import com.recceda.http.Client;
+import com.recceda.http.HttpConstants;
+import com.recceda.http.requests.file.CreateFileRequest;
+import com.recceda.http.response.file.FileContentResponse;
+import com.recceda.mapper.RequestMapper;
+import com.recceda.mapper.ResponseMapper;
+
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.concurrent.ExecutionException;
+
+public class FileAction {
+
+    private final Client client;
+
+    public FileAction(Client client) {
+        this.client = client;
+    }
+
+    public void createFile(CreateFileRequest createFileRequest, String owner, String repo, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
+        executeCreateOrUpdate(createFileRequest, owner, repo, path, 201, "Failed to create file.");
+    }
+
+    public void createFile(CreateFileRequest createFileRequest, Repository repository, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
+        createFile(createFileRequest, repository.getOwner().getLogin(), repository.getName(), path);
+    }
+
+    public void updateFile(CreateFileRequest createFileRequest, String owner, String repo, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
+        executeCreateOrUpdate(createFileRequest, owner, repo, path, 200, "Failed to update file.");
+    }
+
+    private void executeCreateOrUpdate(CreateFileRequest createFileRequest, String owner, String repo, String path, int expectedStatusCode, String errorMessage) throws JsonProcessingException, ExecutionException, InterruptedException {
+        HttpRequest request = client.requestBuilder(buildContentsPath(owner, repo, path))
+                .PUT(HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(createFileRequest)))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
+                .get();
+        if (response.statusCode() != expectedStatusCode) throw new RuntimeException(errorMessage);
+    }
+
+
+    public <T> T getFileContents(String owner, String repo, String path, Class<T> type) throws ExecutionException, InterruptedException, JsonProcessingException {
+        HttpRequest request = client.requestBuilder(buildContentsPath(owner, repo, path))
+                .header("Accept", HttpConstants.GITHUB_RAW_JSON_HEADER)
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
+        if (response.statusCode() != 200) throw new RuntimeException("Failed to get file contents.");
+
+        return ResponseMapper.fromResponse(response, type);
+    }
+
+    public FileContentResponse getFileContents(String owner, String repo, String path) throws ExecutionException, InterruptedException, JsonProcessingException {
+        HttpRequest request = client.requestBuilder(buildContentsPath(owner, repo, path))
+                .build();
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
+        if (response.statusCode() != 200) throw new RuntimeException("Failed to get file contents.");
+        return ResponseMapper.fromResponse(response, FileContentResponse.class);
+    }
+
+    private String buildContentsPath(String owner, String repo, String path) {
+        return ApiPaths.SLASH + ApiPaths.REPOS + ApiPaths.SLASH + owner + ApiPaths.SLASH + repo + ApiPaths.SLASH + ApiPaths.CONTENTS + ApiPaths.SLASH + path;
+    }
+}
--- src/main/java/com/recceda/action/RepositoryAction.java
+++ src/main/java/com/recceda/action/RepositoryAction.java
@@ -3,10 +3,9 @@ package com.recceda.action;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.recceda.elements.Repository;
+import com.recceda.http.ApiPaths;
 import com.recceda.http.Client;
-import com.recceda.http.requests.file.CreateFileRequest;
 import com.recceda.http.requests.repository.CreateRepositoryRequest;
-import com.recceda.http.response.file.FileContentResponse;
 import com.recceda.mapper.RequestMapper;
 import com.recceda.mapper.ResponseMapper;
 
@@ -14,8 +13,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
-
-import static com.recceda.Constants.*;
 
 public class RepositoryAction {
     private final Client client;
@@ -33,7 +30,7 @@ public class RepositoryAction {
      * @return Repository
      */
     public Repository getRepository(final String owner, final String name) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + name).build();
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.REPOS + ApiPaths.SLASH + owner + ApiPaths.SLASH + name).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers
                 .ofString()
         ).get();
@@ -42,7 +39,7 @@ public class RepositoryAction {
     }
 
     public List<Repository> getAllRepositoriesByOwner(final String owner) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH + USERS + SLASH + owner + SLASH + REPOS).build();
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USERS + ApiPaths.SLASH + owner + ApiPaths.SLASH + ApiPaths.REPOS).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
         if (response.statusCode() != 200) List.of();
         return ResponseMapper.fromResponse(response, new TypeReference<List<Repository>>() {
@@ -50,7 +47,7 @@ public class RepositoryAction {
     }
 
     public Repository createRepositoryForAuthenticatedUser(CreateRepositoryRequest request) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest req = client.requestBuilder(SLASH + USER + SLASH + REPOS)
+        HttpRequest req = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USER + ApiPaths.SLASH + ApiPaths.REPOS)
                 .POST(HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(request)))
                 .build();
         HttpResponse<String> response = client.send(req, HttpResponse.BodyHandlers.ofString()).get();
@@ -59,7 +56,7 @@ public class RepositoryAction {
     }
 
     public void deleteRepositoryForAuthenticatedUser(String owner, String name) throws ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + name)
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.REPOS + ApiPaths.SLASH + owner + ApiPaths.SLASH + name)
                 .DELETE()
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
@@ -67,60 +64,4 @@ public class RepositoryAction {
             throw new RuntimeException("Failed to delete repository");
         }
     }
-
-    public void createFile(CreateFileRequest createFileRequest, String owner, String repo, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + repo + SLASH + CONTENTS + SLASH + path)
-                .PUT(HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(createFileRequest)))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
-                .get();
-        if (response.statusCode() != 201) throw new RuntimeException("Failed to create file.");
-    }
-
-    /**
-     *
-     * @param createFileRequest - this is the request to save the file
-     * @param repository        - the repository which the file belongs to
-     * @param path              - the path where the repository is saved to and
-     * @throws JsonProcessingException
-     * @throws ExecutionException
-     * @throws InterruptedException
-     */
-    public void createFile(CreateFileRequest createFileRequest, Repository repository, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + repository.getFullName() + SLASH + CONTENTS + SLASH + path)
-                .PUT(HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(createFileRequest)))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
-                .get();
-        if (response.statusCode() != 201) throw new RuntimeException("Failed to create file.");
-    }
-
-    public void updateFile(CreateFileRequest createFileRequest, String owner, String repo, String path) throws JsonProcessingException, ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + repo + SLASH + CONTENTS + SLASH + path)
-                .PUT(HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(createFileRequest)))
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString())
-                .get();
-        if (response.statusCode() != 200) throw new RuntimeException("Failed to create file.");
-    }
-
-
-    public <T> T getFileContents(String owner, String repo, String path, Class<T> type) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + repo + SLASH + CONTENTS + SLASH + path)
-                .header("Accept", GITHUB_RAW_JSON_HEADER)
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
-        if (response.statusCode() != 200) throw new RuntimeException("Failed to get file contents.");
-
-        return ResponseMapper.fromResponse(response, type);
-    }
-
-    public FileContentResponse getFileContents(String owner, String repo, String path) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH + REPOS + SLASH + owner + SLASH + repo + SLASH + CONTENTS + SLASH + path)
-                .build();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
-        if (response.statusCode() != 200) throw new RuntimeException("Failed to get file contents.");
-        return ResponseMapper.fromResponse(response, FileContentResponse.class);
-    }
-
 }
