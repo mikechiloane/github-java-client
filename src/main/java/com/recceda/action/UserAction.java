a2418ea5b048ce17f64ad232341f8f8707ac5151
Mike Nhlanhla Chiloane <38855352+mikechiloane@users.noreply.github.com>
2025-10-21 23:51:15 +0200

    Refactor

--- src/main/java/com/recceda/action/UserAction.java
+++ src/main/java/com/recceda/action/UserAction.java
@@ -2,7 +2,9 @@ package com.recceda.action;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.recceda.elements.Owner;
+import com.recceda.http.ApiPaths;
 import com.recceda.http.Client;
+import com.recceda.http.HttpConstants;
 import com.recceda.http.requests.user.UpdateUserRequest;
 import com.recceda.mapper.RequestMapper;
 import com.recceda.mapper.ResponseMapper;
@@ -11,8 +13,6 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.concurrent.ExecutionException;
 
-import static com.recceda.Constants.*;
-
 public class UserAction {
     private final Client client;
 
@@ -20,21 +20,22 @@ public class UserAction {
         this.client = client;
     }
 
-    public  Owner getUser(String username) throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH+USER+SLASH+username).build();
+    public Owner getUser(String username) throws ExecutionException, InterruptedException, JsonProcessingException {
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USERS + ApiPaths.SLASH + username).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
-        if(response.statusCode() != 200) throw new RuntimeException("User not found");
+        if (response.statusCode() != 200) throw new RuntimeException("User not found");
         return ResponseMapper.fromResponse(response, Owner.class);
     }
 
     public Owner getAuthenticatedUser() throws ExecutionException, InterruptedException, JsonProcessingException {
-        HttpRequest request = client.requestBuilder(SLASH + USER).build();
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USER).build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
         return ResponseMapper.fromResponse(response, Owner.class);
     }
 
-    public void starRepository(String path) throws ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + USER + SLASH + STARRED + path)
+    public void starRepository(String owner, String repo) throws ExecutionException, InterruptedException {
+        String path = ApiPaths.SLASH + ApiPaths.USER + ApiPaths.SLASH + ApiPaths.STARRED + ApiPaths.SLASH + owner + ApiPaths.SLASH + repo;
+        HttpRequest request = client.requestBuilder(path)
                 .PUT(HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
@@ -42,15 +43,15 @@ public class UserAction {
     }
 
     public Owner updateAuthenticatedUser(UpdateUserRequest updateUserRequest) throws JsonProcessingException, ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH+USER)
-                .method(PATCH, HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(updateUserRequest)))
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USER)
+                .method(HttpConstants.PATCH, HttpRequest.BodyPublishers.ofString(RequestMapper.toJson(updateUserRequest)))
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
         return ResponseMapper.fromResponse(response, Owner.class);
     }
 
     public void followUser(String username) throws ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + USER + FOLLOWING + SLASH + username)
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USER + ApiPaths.SLASH + ApiPaths.FOLLOWING + ApiPaths.SLASH + username)
                 .PUT(HttpRequest.BodyPublishers.noBody())
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
@@ -58,7 +59,7 @@ public class UserAction {
     }
 
     public void unfollowUser(String username) throws ExecutionException, InterruptedException {
-        HttpRequest request = client.requestBuilder(SLASH + USER + FOLLOWING + SLASH + username)
+        HttpRequest request = client.requestBuilder(ApiPaths.SLASH + ApiPaths.USER + ApiPaths.SLASH + ApiPaths.FOLLOWING + ApiPaths.SLASH + username)
                 .DELETE()
                 .build();
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString()).get();
